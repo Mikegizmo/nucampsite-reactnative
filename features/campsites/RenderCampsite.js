8ac08cb8262dc3867266363d93f981c2cfa04c80
Michael Panter <makinglife1231@gmail.com>
2022-12-16 23:34:04 -0500

    week 2 workshop

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,9 +1,11 @@
+import { Modal } from "react-native";
 import { Text, View, StyleSheet } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from "../../shared/baseUrl";
 
 const RenderCampsite = (props) => {
   const { campsite } = props;
+  
   if(campsite) {
     return (
       <Card style={styles.cardContainer}>
@@ -22,34 +24,35 @@ const RenderCampsite = (props) => {
                 backgroundColor: 'rgba(169, 169, 169, 0.6)'
               }}
             >
-              <Text
-                style={{
-                  color: 'white',
-                  textAlign: 'center',
-                  fontSize: 20,
-                  textShadowColor: 'black',
-                  textShadowOffset: {width: 2, height: 2},
-                  textShadowRadius: 1
-                }}
-              >
+              <Text style={styles.cardText}>
                 {campsite.name}
               </Text>
             </View>
           </View>
         </Card.Image>
         <Text style={{ margin: 20 }}>{campsite.description}</Text>
-        <Icon
-          name={props.isFavorite ? 'heart' : 'heart-o'}
-          type='font-awesome'
-          color='#f50'
-          raised
-          reverse
-          onPress={() =>
-            props.isFavorite
-              ? console.log('Already set as a favorite')
-              : props.markFavorite()
-          }
-        />
+        <View style={styles.cardRow}>
+          <Icon
+            name={props.isFavorite ? 'heart' : 'heart-o'}
+            type='font-awesome'
+            color='#f50'
+            raised
+            reverse
+            onPress={() =>
+              props.isFavorite
+                ? console.log('Already set as a favorite')
+                : props.markFavorite()
+            }
+          />
+          <Icon 
+            name='pencil'
+            type='font-awesome'
+            color='#5637DD'
+            raised
+            reverse
+            onPress={() => props.onShowModal()}
+          />
+        </View>
       </Card>
     );
   }
@@ -61,6 +64,21 @@ const styles = StyleSheet.create({
     padding: 0,
     margin: 0,
     marginBottom: 20
+  },
+  cardRow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    margin: 20
+  },
+  cardText: {
+    textShadowColor: 'rgba(0,0,0,1)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 20,
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 20
   }
 });
 
